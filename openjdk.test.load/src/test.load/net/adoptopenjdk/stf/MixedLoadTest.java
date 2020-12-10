5b4f7271fe881c271bc6ac2f38941df6e2548799
Mesbah_Alam@ca.ibm.com <Mesbah_Alam@ca.ibm.com>
2020-12-10 16:35:00 -0500

    Add MiniMix_5min target

--- openjdk.test.load/src/test.load/net/adoptopenjdk/stf/MixedLoadTest.java
+++ openjdk.test.load/src/test.load/net/adoptopenjdk/stf/MixedLoadTest.java
@@ -22,15 +22,34 @@ import net.adoptopenjdk.stf.processes.ExpectedOutcome;
 import net.adoptopenjdk.stf.processes.definitions.JavaProcessDefinition;
 import net.adoptopenjdk.stf.processes.definitions.LoadTestProcessDefinition;
 import net.adoptopenjdk.stf.runner.modes.HelpTextGenerator;
+import net.adoptopenjdk.stf.util.TimeParser;
 import net.adoptopenjdk.loadTest.InventoryData;
+import net.adoptopenjdk.stf.environment.StfTestArguments; 
 
 public class MixedLoadTest implements StfPluginInterface {
+	
+	String timeLimit;
+	int testCountMultiplier;
+	boolean isTimeBasedLoadTest = true;
+	String defaultTimeout = "3h";
+	
 	public void help(HelpTextGenerator help) throws StfException {
 		help.outputSection("MixedLoadTest runs workloads of math,mauve,nio,lang,concurrent unit tests");
 		help.outputText("");
 	}
 
 	public void pluginInit(StfCoreExtension stf) throws StfException {
+		// When used as a time based test, this STF test plugin accepts the following two parameters:
+		//   (1) timeLimit     	     : Time duration (e.g. 5s, 5m, 5h) for which the load should be run. 
+		//   (2) testCountMultiplier : Optional multiplier value to increase the workload externally.
+		StfTestArguments testArgs = stf.env().getTestProperties("timeLimit=[notset]","testCountMultiplier=[1]");
+		timeLimit = testArgs.get("timeLimit");
+		testCountMultiplier = Integer.parseInt(testArgs.get("testCountMultiplier"));
+		
+		// When We are using MixedLoadTest for default cases;. e.g. not in a time based load test scenario 
+		if ( timeLimit.equals("notset")) {
+			isTimeBasedLoadTest = false; 
+		}
 	}
 
 	public void setUp(StfCoreExtension test) throws StfException {
@@ -48,9 +67,7 @@ public class MixedLoadTest implements StfPluginInterface {
 		if (!fileSystemJar.asJavaFile().exists()) {
 			throw new StfException("The filesystem.jar does not exist: " + fileSystemJar.getSpec());
 		}
-		
-		String timeout = "3h";
-		
+
 		String inventoryFile = "/openjdk.test.load/config/inventories/mix/mix-more.xml";
 		int totalTests = InventoryData.getNumberOfTests(test, inventoryFile);
 		int cpuCount = Runtime.getRuntime().availableProcessors();
@@ -63,16 +80,37 @@ public class MixedLoadTest implements StfPluginInterface {
 				.addProjectToClasspath("openjdk.test.nio")
 				.addProjectToClasspath("openjdk.test.concurrent")
 				.addJarToClasspath(mauveJar)
-				.addJarToClasspath(fileSystemJar)
-				.setAbortIfOutOfMemory(false)
-				.addSuite("mix")
-				.setSuiteThreadCount(cpuCount - 2, 10)	
-				.setSuiteNumTests(totalTests * 200) // Leave 1 cpu for the JIT. i for GC and set min 20			
-				.setSuiteInventory(inventoryFile) 			// Point at the file which lists the tests
-				.setSuiteRandomSelection();		  			// Randomly pick the next test each time
+				.addJarToClasspath(fileSystemJar);
 				
+		if (isTimeBasedLoadTest) { 
+			loadTestInvocation = loadTestInvocation.setTimeLimit(timeLimit);	// If it's a time based test, stop execution after given time duration
+		}
+		loadTestInvocation = loadTestInvocation.setAbortIfOutOfMemory(false)
+				.addSuite("mix")
+				.setSuiteThreadCount(cpuCount - 2, 10); // Leave 1 cpu for the JIT, 1 cpu for GC and set min 10
+		if (isTimeBasedLoadTest) { 
+			loadTestInvocation = loadTestInvocation
+				.setSuiteNumTests(totalTests * 80000000 * testCountMultiplier); // If it's a time based test, run a very large load
+		} else {  
+			loadTestInvocation = loadTestInvocation
+				.setSuiteNumTests(totalTests * 200);
+		}
+		
+		loadTestInvocation = loadTestInvocation
+				.setSuiteInventory(inventoryFile) 		// Point at the file which lists the tests
+				.setSuiteRandomSelection(); 			// Randomly pick the next test each time
+		
+		String finalTimeout = defaultTimeout;
+		
+		// If it's a time-based test, set timeout for the test process to the default timeout(3h) + given timeLimit
+		if (isTimeBasedLoadTest) { 
+			long finalTimeoutInSeconds = TimeParser.parseTimeSpecification(defaultTimeout).getSeconds() + 
+					TimeParser.parseTimeSpecification(timeLimit).getSeconds();
+			finalTimeout = finalTimeoutInSeconds + "s"; 
+		}
+		
 		test.doRunForegroundProcess("Run mixed unit tests", "LT", Echo.ECHO_ON,
-				ExpectedOutcome.cleanRun().within(timeout), 
+				ExpectedOutcome.cleanRun().within(finalTimeout), 
 				loadTestInvocation);		
 			
 	}

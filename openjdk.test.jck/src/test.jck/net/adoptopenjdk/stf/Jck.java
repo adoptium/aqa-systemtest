d2195e7d2724a53e27a0ef8d132c01e4c4112b75
George Adams <georgeadams1995@gmail.com>
2021-07-23 10:34:32 +0100

    skip setting localHostIPAddr on Solaris

--- openjdk.test.jck/src/test.jck/net/adoptopenjdk/stf/Jck.java
+++ openjdk.test.jck/src/test.jck/net/adoptopenjdk/stf/Jck.java
@@ -668,7 +668,10 @@ public class Jck implements StfPluginInterface {
 			}	
 			if ( tests.contains("api/java_net") || tests.contains("api/java_nio") || tests.equals("api") ) {
 				fileContent += "set jck.env.runtime.net.localHostName " + hostname + ";\n";
-				fileContent += "set jck.env.runtime.net.localHostIPAddr " + ipAddress + ";\n";
+				// The Solaris tests fail with this: specified tag "jck.env.runtime.net.localHostIPAddr" was not found on path. The path is:jck.intro
+				if (!platform.equals("solaris")) {
+					fileContent += "set jck.env.runtime.net.localHostIPAddr " + ipAddress + ";\n";
+				}
 				fileContent += "set jck.env.runtime.net.testHost1Name " + testHost1Name + ";\n";
 				fileContent += "set jck.env.runtime.net.testHost1IPAddr " + testHost1Ip + ";\n";
 				fileContent += "set jck.env.runtime.net.testHost2Name " + testHost2Name + ";\n";

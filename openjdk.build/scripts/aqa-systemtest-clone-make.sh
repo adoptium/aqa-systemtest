ab40a4d17d1fd4125cac17e177f43bf6a79717bc
Keith W. Campbell <keithc@ca.ibm.com>
2021-08-19 17:34:32 -0400

    Update references from AdoptOpenJDK to adoptium (#449)
    * Tidy up whitespace
    
    * indent reasonably and consistently
    * remove trailing whitespace
    * collapse multiple blank lines to one
    * use CR/LF for *.bat and *.cmd files
    
    Signed-off-by: Keith W. Campbell <keithc@ca.ibm.com>
    
    * Update references from AdoptOpenJDK to adoptium
    
    * openjdk-systemtest is now aqa-systemtest
    * rename scripts accordingly
    
    Signed-off-by: Keith W. Campbell <keithc@ca.ibm.com>

--- openjdk.build/scripts/aqa-systemtest-clone-make.sh
+++ openjdk.build/scripts/aqa-systemtest-clone-make.sh
@@ -16,26 +16,26 @@
 # Clone stf
 mkdir -p $HOME/git && cd $HOME/git && rm -rf stf && mkdir stf && git clone https://github.com/adoptium/stf.git stf
 if [ "$?" != "0" ]; then
-        echo "Error cloning stf" 1>&2
-        exit 1
+    echo "Error cloning stf" 1>&2
+    exit 1
 fi
 # Clone aqa-systemtest
 mkdir -p $HOME/git && cd $HOME/git && rm -rf aqa-systemtest && mkdir aqa-systemtest && git clone https://github.com/adoptium/aqa-systemtest.git aqa-systemtest
 if [ "$?" != "0" ]; then
-        echo "Error cloning aqa-systemtest" 1>&2
-        exit 1
+    echo "Error cloning aqa-systemtest" 1>&2
+    exit 1
 fi
 # Configure (get prereqs)
 cd $HOME/git/aqa-systemtest/openjdk.build/ && make configure
 if [ "$?" != "0" ]; then
-        echo "Error configuring aqa-systemtest - see build output" 1>&2
-        exit 1
+    echo "Error configuring aqa-systemtest - see build output" 1>&2
+    exit 1
 fi
 # Build
 cd $HOME/git/aqa-systemtest/openjdk.build/ && make
 if [ "$?" != "0" ]; then
-        echo "Error building aqa-systemtest - see build output" 1>&2
-        exit 1
+    echo "Error building aqa-systemtest - see build output" 1>&2
+    exit 1
 fi
 echo "aqa-systemtest repository build successful - to run the tests"
 echo "make -f $HOME/git/aqa-systemtest/openjdk.build/makefile test"
